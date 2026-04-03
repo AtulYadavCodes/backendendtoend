b93d51239cc39ab2b69a074021785e72d5b1ce1e
Atul Yadav <atul99yadav99@gmail.com>
2026-04-03 19:22:40 +0530

    Remove app.js, index.js, and constant.js; update package.json and package-lock.json to reflect new dependencies and entry point

--- index.js
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config';
-import connectDB from "./db/index.js";
-import app from './app.js';
-
-connectDB()
-.then(()=>{app.listen(8000||process.env.PORT,()=>{console.log(`Server started on port ${8000||process.env.PORT}`)})})
-.catch((err)=>{console.log(err)});
