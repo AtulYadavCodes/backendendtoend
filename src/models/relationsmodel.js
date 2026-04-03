a6b66b1e54744479f6373965ff9d39e3a14d7284
Atul Yadav <atul99yadav99@gmail.com>
2026-04-03 15:50:23 +0530

    Remove Group and PDF models; update Relation model to reference Image instead of user

--- src/models/relationsmodel.js
+++ src/models/relationsmodel.js
@@ -2,29 +2,19 @@ import mongoose from 'mongoose';
 
 const relationSchema = new mongoose.Schema(
   {
-    user: {
+    Image: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
-      required: true,
     },
     group: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Group',
-      required: true,
-    },
-    relationType: {
-      type: String,
-      enum: ['member', 'admin', 'owner'],
-      default: 'member',
     },
   },
   {
     timestamps: true,
   }
 );
-
-relationSchema.index({ user: 1, group: 1 }, { unique: true });
-
 const Relation = mongoose.model('Relation', relationSchema);
 
 export default Relation;
