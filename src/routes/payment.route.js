04bc13a1184e525dac22beb8180ca44b0f6ae00d
Atul Yadav <atul99yadav99@gmail.com>
2026-04-22 00:07:04 +0530

    Implement payment routes by linking createorder and verifypayment handlers

--- src/routes/payment.route.js
+++ src/routes/payment.route.js
@@ -1,7 +1,8 @@
 import {Router} from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { createorder, verifypayment } from "../controllers/payment.controller.js";
 
 const router=Router()
-router.route('/createorder').post(verifyJWT,);
-router.route('/verifypayment').post(verifyJWT,);
+router.route('/createorder').post(verifyJWT,createorder);
+router.route('/verifypayment').post(verifyJWT,verifypayment);
 export default router
